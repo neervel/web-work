7af221834312f7e529135f988b51f8b227a4c379
neervel <klihopoy@mail.ru>
2020-05-19 17:50:10 +0300

    final maket

--- js/modal.js
+++ js/modal.js
@@ -1,31 +1,3 @@
 $(function(){
-  const callMeBtn = $('#call-me'),
-    callMe = $('#modal-callme'),
-    kitchen = $('#kitchen'),
-    showerRoom = $('#shower-room'),
-    flat = $('#flat'),
-    office = $('#office'),
-    newBuild = $('#new-build'),
-    close = $('#close');
 
-  function showModal(){
-    callMe.fadeIn(100);
-    callMe.css('display', 'flex');
-  };
-
-  callMeBtn.on('click', ()=> showModal() );
-  kitchen.on('click', ()=> showModal() );
-  showerRoom.on('click', ()=> showModal() );
-  flat.on('click', ()=> showModal() );
-  office.on('click', ()=> showModal() );
-  newBuild.on('click', ()=> showModal() );
-
-
-  close.on('click', function(){
-    $(this).parent().parent().fadeOut(100);
-  });
-
-  
-
-  
 });
